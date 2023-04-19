1183ddcd6dcb987d45d24f14ddac011271f514b3
aegroto <aegroto@protonmail.com>
2023-04-19 21:23:10 +0200

    root

---
+++ src/test/java/co/develhope/java10/helloswagger/HelloSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.java10.helloswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
